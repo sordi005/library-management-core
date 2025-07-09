a4a337113698e206c2a1cc8fbdc4487a68eac01e
Santiago <sordisantiago5@gmail.com>
2025-07-09 20:57:09 -0300

    Actualiza pruebas de persistencia y agrega archivo de configuración de base de datos

--- src/test/java/com/libraryManagement/model/BookTest.java
+++ src/test/java/com/libraryManagement/model/BookTest.java
@@ -17,6 +17,7 @@ public class BookTest extends AbstractPersistenceTest {
         Session session = sessionFactory.openSession();
         Transaction tx = session.beginTransaction();
         try {
+            //Creacion de entidades relacionadas
             log("Creando entidades relacionadas...");
 
             Genre genre = Genre.builder().name("Ficcion").build();
@@ -30,33 +31,32 @@ public class BookTest extends AbstractPersistenceTest {
                     .subTitle("Primera entrega de la saga")
                     .publicationDate(LocalDate.of(1999, 5, 1))
                     .language("ES")
+                    .isbn("978-9878000001")
+                    .edition("1ª edición en español")
+                    .genre(genre)
                     .pageCount(256)
+                    .seriesName("Harry Potter")
+                    .seriesOrder(1)
+                    .originalLanguage("EN")
                     .summary("""
                                Harry descubre que es un mago y que está destinado a asistir a Hogwarts, una escuela mágica donde entabla amistad con Ron y Hermione, 
                                y enfrentará los misterios de la piedra filosofal y el regreso del temido Lord Voldemort.
                             """)
-                    .isbn("978-9878000001").seriesName("Harry Potter")
-                    .seriesOrder(1)
-                    .edition("1ª edición en español")
-                    .originalLanguage("EN")
                     .tableOfContents("""
                                Capítulo 1: El niño que vivió
                                Capítulo 2: El vidrio que desapareció
-                               Capítulo 3: Carta de Hogwarts 
-                               ......  
+                               ...
+                               Capítulo final: El hombre con dos caras
                             """)
                     .build();
 
             log("Estableciendo relaciones bidireccionales...");
-
-            book.setGenre(genre); // Book ↔ Genre
-            book.setPublisher(publisher); //  Book ↔ Publisher
             book.addAuthor(author);         // Book ↔ Author
-            book.addCopy(copy);             // Book ↔ Copy
+            book.addCopy(copy);            // Book ↔ Copy
+            publisher.addPublication(book); // Publisher ↔ Book
 
-            log("Persistiendo entidad Book (cascade hacia Copy, Author, Genre, Publisher)...");
-
-            session.persist(publisher); //Cascade : Publication -> Author, Genre, Copy */
+            log("Persistiendo entidad Publisher (cascade hacia Book : Genre,Author,Copy  )...");
+            session.persist(publisher); // Cascade : -> Book -->(Genre, Author, Copy)
 
             tx.commit();
             session.clear(); // Limpiar caché para simular acceso desde cero
