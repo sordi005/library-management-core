82f9350d42cb653b0f784245bb51e060845b4a2a
Santiago <sordisantiago5@gmail.com>
2025-07-14 20:39:36 -0300

    Implementa métodos en AddressMapper para manejar conversiones entre Address y AddressDTO, incluyendo actualizaciones y listas

--- src/main/java/com/libraryManagement/mapper/AddressMapper.java
+++ src/main/java/com/libraryManagement/mapper/AddressMapper.java
@@ -1,4 +1,28 @@
 package com.libraryManagement.mapper;
 
-public class AddressMapper {
+import com.libraryManagement.dto.AddressDTO;
+import com.libraryManagement.model.Address;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(
+    componentModel = "default",
+    unmappedTargetPolicy = ReportingPolicy.IGNORE,
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
+public interface AddressMapper {
+
+    AddressMapper INSTANCE = Mappers.getMapper(AddressMapper.class);
+
+    AddressDTO toDTO(Address address);
+
+    @Mapping(target = "id", ignore = true)
+    Address toEntity(AddressDTO addressDTO);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDTO(AddressDTO addressDTO, @MappingTarget Address address);
+
+    List<AddressDTO> toDTOs(List<Address> addresses);
 }
