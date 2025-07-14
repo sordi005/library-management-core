72d7ed5340210d988e4d946c8b0d50a1397d9164
Santiago <sordisantiago5@gmail.com>
2025-07-14 20:39:25 -0300

    Añade validaciones y anotaciones de Lombok a la clase AddressDTO para mejorar la gestión de datos de dirección

--- src/main/java/com/libraryManagement/dto/AddressDTO.java
+++ src/main/java/com/libraryManagement/dto/AddressDTO.java
@@ -1,4 +1,36 @@
 package com.libraryManagement.dto;
 
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class AddressDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Street is required")
+    @Size(max = 30, message = "Street cannot exceed 30 characters")
+    private String street;
+
+    @NotBlank(message = "Number is required")
+    @Size(max = 10, message = "Number cannot exceed 10 characters")
+    private String number;
+
+    @NotBlank(message = "City is required")
+    @Size(max = 50, message = "City cannot exceed 50 characters")
+    private String city;
+
+    @Size(max = 50, message = "Province cannot exceed 50 characters")
+    private String province;
+
+    @NotBlank(message = "Country is required")
+    @Size(max = 50, message = "Country cannot exceed 50 characters")
+    private String country;
+
+    @NotBlank(message = "Postal code is required")
+    @Size(max = 10, message = "Postal code cannot exceed 10 characters")
+    @Pattern(regexp = "^[0-9A-Za-z\\s-]+$", message = "Invalid postal code format")
+    private String postalCode;
 }
