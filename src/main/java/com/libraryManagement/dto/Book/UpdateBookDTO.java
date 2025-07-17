7413eac2536e9a66a75ce56b34125af1832b81a9
Santiago <sordisantiago5@gmail.com>
2025-07-17 20:00:22 -0300

    Añade la clase `UpdateBookDTO` con validaciones, anotaciones de Lombok y herencia de `BaseUpdatePublicationDTO` para gestionar la actualización de datos de libros.

---
+++ src/main/java/com/libraryManagement/dto/Book/UpdateBookDTO.java
@@ -0,0 +1,28 @@
+package com.libraryManagement.dto.Book;
+
+import com.libraryManagement.dto.Publication.BaseUpdatePublicationDTO;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+public class UpdateBookDTO extends BaseUpdatePublicationDTO {
+    @NotBlank(message = "ISBN is required")
+    @Size(max = 13, message = "ISBN cannot exceed 13 characters")
+    private String isbn;
+
+    @Size(max = 100, message = "Series name cannot exceed 100 characters")
+    private String seriesName;
+
+    @Positive(message = "Series order must be positive")
+    private Integer seriesOrder;
+
+    @Size(max = 30, message = "Original language cannot exceed 30 characters")
+    private String originalLanguage;
+
+    @Size(max = 2000, message = "Table of contents cannot exceed 2000 characters")
+    private String tableOfContents;
+}
