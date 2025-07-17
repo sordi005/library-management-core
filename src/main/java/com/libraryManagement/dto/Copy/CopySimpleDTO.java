db50a04d2fe6be1aac3e6a629e4141e0d4e33b4a
Santiago <sordisantiago5@gmail.com>
2025-07-17 20:22:22 -0300

    Añade las clases `UpdateCopyDTO`, `CopyDetailDTO`, `CreateCopy` y `CopySimpleDTO` con validaciones, anotaciones de Lombok y atributos necesarios para la gestión de datos de ejemplares.

---
+++ src/main/java/com/libraryManagement/dto/Copy/CopySimpleDTO.java
@@ -0,0 +1,16 @@
+package com.libraryManagement.dto.Copy;
+
+import com.libraryManagement.model.enums.CopyStatus;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CopySimpleDTO {
+    private Long id;
+    private Integer copyNumber;
+    private CopyStatus status;
+    private String publicationTitle;
+    private String publicationType;
+
+}
---
+++ src/main/java/com/libraryManagement/dto/Copy/UpdateCopyDTO.java
@@ -0,0 +1,18 @@
+package com.libraryManagement.dto.Copy;
+
+import com.libraryManagement.model.enums.CopyStatus;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UpdateCopyDTO {
+    @NotNull(message = "Copy number is required")
+    @Positive(message = "Copy number must be positive")
+    private Integer copyNumber;
+
+    @NotNull(message = "Status is required")
+    private CopyStatus status;
+}
